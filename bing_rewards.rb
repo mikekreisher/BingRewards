a4062c149061cbe1fa944399d31e19fa00c178ca
mikekreisher <mike.kreisher@gmail.com>
2013-12-31 03:12:09 -0500

    Reorganized and made searching part of ToDo

--- bing_rewards.rb
+++ bing_rewards.rb
@@ -10,6 +10,7 @@ browser_path   = ""
 approve_topics = false
 search_count   = 30
 errors 		   = false
+searches_per_credit = 3
 
 if ARGV.count == 1 && File.exists?(ARGV[0])
   config_file = File.open(ARGV[0], "r")
@@ -27,34 +28,69 @@ if ARGV.count == 1 && File.exists?(ARGV[0])
         approve_topics = split_line[1]
       when "[search_count]"
         search_count = split_line[1].to_i
+      when "[searches_per_credit]"
+        searches_per_credit = split_line[1].to_i
       end
     end
   end
   config_file.close
 end
 
-topics_doc = Nokogiri::HTML(open('http://soovle.com/top'))
-topics     = topics_doc.search('div.letter .correction span').to_a.sample(search_count).collect{|x| x.content}
-topics.shuffle!
-print "Found #{search_count} Search Topics\n"
+def search(credits, searches_per_credit, approve_topics, browser)
+  begin
+    print "Gathering Searches...\n"
+    search_count = credits * searches_per_credit
+    topics_doc = Nokogiri::HTML(open('http://soovle.com/top'))
+    topics     = topics_doc.search('div.letter .correction span').to_a.sample(search_count).collect{|x| x.content}
+    topics.shuffle!
+    print "Found #{search_count} Search Topics\n"
+  rescue OpenURI::HTTPError => e
+    raise IOError, "Unable to find search topics"
+  end
+  
 
-if approve_topics
-  topics_approved = false
-  while !topics_approved
-    print "=============\nSEARCH TOPICS\n=============\n"
-    topics.each_with_index do |topic, i|
-      print "#{(i+1).to_s.rjust(2)}. #{topic}\n"
-    end
-    print "=============\n=============\n"
-    puts "Do you approve these topics? (y|n):"
-    if STDIN.gets.chomp.downcase == "y"
-      topics_approved = true
-    else
-      topics = topics_doc.search('div.letter .correction span').to_a.sample(search_count).collect{|x| x.content}
-      topics.shuffle!
+  if approve_topics
+    topics_approved = false
+    while !topics_approved
+      print "=============\nSEARCH TOPICS\n=============\n"
+      topics.each_with_index do |topic, i|
+        print "#{(i+1).to_s.rjust(2)}. #{topic}\n"
+      end
+      print "=============\n=============\n"
+      puts "Do you approve these topics? (y|n):"
+      if STDIN.gets.chomp.downcase == "y"
+        topics_approved = true
+      else
+        topics = topics_doc.search('div.letter .correction span').to_a.sample(search_count).collect{|x| x.content}
+        topics.shuffle!
+      end
     end
   end
+
+  begin
+    topics.each_with_index do |topic, i|
+      print "#{(i+1).to_s.rjust(2)}. Searching for #{topic}\n"
+      browser.text_field(:id=>"sb_form_q").when_present.set(topic)
+      browser.form(:id=>"sb_form").submit
+      sleep 5 # Wait 5 seconds
+    end
+    print "\n==================\nSEARCHES COMPLETED\n==================\n"
+  rescue Watir::Exception::UnknownFormException => e
+    print "\n*****\nERROR\n*****\n"
+    print "There was an error performing the searches:\n#{e.message}\n"
+    raise Watir::Exception::WatirException, "Could not find form"
+    errors = true
+  rescue Watir::Exception::TimeoutException => e
+    print "\n*****\nERROR\n*****\n"
+    print "There was an error performing the searches:\n#{e.message}\n"
+    raise Watir::Exception::WatirException, "Timeout Occurred"
+  end
+
 end
+
+
+
+
 
 unless browser_path == ""
    Selenium::WebDriver::Firefox::Binary.path = browser_path
@@ -65,6 +101,7 @@ b.goto 'bing.com'
 b.span(:text=>"Sign in").when_present.click
 b.link(:href, /login\.live/).when_present.click
 
+print "Logging In\n"
 begin
   login          = b.text_field :type => 'email', :name => 'login'
   pass           = b.text_field :type => 'password', :name => 'passwd'
@@ -86,21 +123,9 @@ begin
   sign_in_button.click
   b.alert.when_present.ok
 end while(login.exists? && pass.exists? && sign_in_button.exists?)
+print "Logged in as #{username}\n"
 
-begin
-  topics.each_with_index do |topic, i|
-    print "#{(i+1).to_s.rjust(2)}. Searching for #{topic}\n"
-    b.text_field(:id=>"sb_form_q").when_present.set(topic)
-    #b.input(:type=>'submit', :id=>'sb_form_go').click
-    b.form(:id=>"sb_form").submit
-    sleep 5 # Wait 5 seconds
-  end
-  print "\n==================\nSEARCHES COMPLETED\n==================\n"
-rescue Exception => e
-  print "\n*****\nERROR\n*****\n"
-  print "There was an error performing the searches:\n#{e.message}\n"
-  errors = true
-end
+
 
 print "\n=========\nTODO LIST\n=========\n"
 b.goto 'http://www.bing.com/rewards/dashboard'
@@ -117,9 +142,18 @@ begin
 	todo_ids.each do |id|
 		link_to_click = b.div(:class=>'tileset').ul(:class=>'row').link(:id=>id)
 		print "- #{link_to_click.text}\n"
-		link_to_click.click
-		b.windows.last.use
-		b.windows.last.close
+    if link_to_click.href == "http://www.bing.com/search?q=weather&bnprt=searchandearn"
+      progress_tile = link_to_click.div(:class=>'progress')
+      progress = progress_tile.text.match(/^(\d+) of (\d+) credits$/)
+      link_to_click.click
+      b.windows.last.use
+      search(progress[2].to_i - progress[1].to_i, searches_per_credit, approve_topics, b)
+      b.windows.last.close
+    else
+      link_to_click.click
+      b.windows.last.use
+      b.windows.last.close
+    end
 		b.windows.last.use
 	end
 rescue Exception => e
